8c35710f56705bfaa867ed26c610f04230475e76
Thomas Klein <thomas.klein.1982@gmail.com>
2022-06-29 01:16:53 +0200

    text

--- src/components/Info-Dialog.vue
+++ src/components/Info-Dialog.vue
@@ -2,6 +2,7 @@
   <Dialog v-model:visible="show" header="Über diese Website">
     <p>Auf dieser Website finden Sie einen großen Teil der Projektarbeiten, die Schüler:innen der Jahrgangsstufen 9, 10 und der E-Phase der <a href="https://marienschule-limburg.de" target="_blank">Marienschule</a> im Informatik-Unterricht selbst programmiert haben.</p>
     <p>Die verwendeten Programmiersprachen sind Javascript im WU-Unterricht und Java in der Oberstufe. Gearbeitet haben die Schüler:innen mit <a href="https://thomaskl.uber.space/Apps/java-app/" target="_blank">JSEdit</a> bzw. <a href="https://thomaskl.uber.space/Apps/java-app/" target="_blank">JavaApp</a>.</p>
+    <p>Viele der Spiele lassen sich auch mit einem angeschlossenen Bluetooth- oder USB-Gamepad spielen.</p>
     <p>Viel Spaß beim Spielen und Entdecken!</p>
     <p>Thomas Klein (klein.t"at"marienschule-limburg.de)</p>
     <p>Disclaimer: Die Schüler:innen hosten die verlinkten Projekte selbst, daher kann keine Haftung über die verlinkten Inhalte übernommen werden.</p>
